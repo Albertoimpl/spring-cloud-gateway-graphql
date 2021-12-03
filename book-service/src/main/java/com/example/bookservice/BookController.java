c0ea13a7bef14d8357ccc9bf2aebefb0b1b6e80a
Ignacio Lozano <ilozano@vmware.com>
2021-12-03 12:55:25 +0100

    Add Fixtures for books

--- book-service/src/main/java/com/example/bookservice/BookController.java
+++ book-service/src/main/java/com/example/bookservice/BookController.java
@@ -1,20 +1,24 @@
 package com.example.bookservice;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class BookController {
 
     private static List<Book> ALL_BOOKS;
 
-    public BookController() {
-        ALL_BOOKS = new ArrayList<>();
-        ALL_BOOKS.add(new Book("9780007119271", "The Mysterious Affair at Styles", 296, List.of("Agatha Christie")));
-        ALL_BOOKS.add(new Book("9780008196523", "The Thirteen Problems", 256, List.of("Agatha Christie")));
+    public BookController() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ALL_BOOKS = mapper.readValue(new ClassPathResource("./fixtures/books.json").getFile(), new TypeReference<>() {});
     }
 
     @GetMapping("books")
