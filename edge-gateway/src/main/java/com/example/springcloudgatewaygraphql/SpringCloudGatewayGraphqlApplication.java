0168348e11e03ee683f0c3ea9097273f01caf0df
Alberto C. Ríos <albertoimpl@gmail.com>
2021-12-03 10:31:29 +0100

    Initial commit

---
+++ edge-gateway/src/main/java/com/example/springcloudgatewaygraphql/SpringCloudGatewayGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewaygraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayGraphqlApplication.class, args);
+    }
+
+}
---
+++ edge-gateway/src/test/java/com/example/springcloudgatewaygraphql/SpringCloudGatewayGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewaygraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
