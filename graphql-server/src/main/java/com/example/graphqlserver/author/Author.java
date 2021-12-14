89fccadc5a57166aaec3f1c4497973c7a1de5063
Ignacio Lozano <ilozano@vmware.com>
2021-12-14 12:51:42 +0100

    Fix authors data fetcher and error when no ID

--- graphql-server/src/main/java/com/example/graphqlserver/author/Author.java
+++ graphql-server/src/main/java/com/example/graphqlserver/author/Author.java
@@ -1,5 +1,8 @@
 package com.example.graphqlserver.author;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Author {
 
     private String firstName;
